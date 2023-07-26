da4674875d048c85f5f602871d01b0e961a10993
sebbyapudo <sebbyapudo@gmail.com>
2023-07-27 02:32:58 +0300

    Dynamically rendered the products mark up

--- 01-cart_house-Eccomerce/app.js
+++ 01-cart_house-Eccomerce/app.js
@@ -16,16 +16,50 @@ let cart = []
 
 // getting products 
 class Products {
-
+  async getProducts(){
+    try {
+      let result = await fetch('products.json')
+      let data = await result.json();
+      let products = data.items;
+      products = products.map(item => {
+        const {title,price} = item.fields;
+        const {id} = item.sys;
+        const img = item.fields.image.fields.file.url;
+        return {title,price,id,img};
+      })
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 // Display Products 
 class UI {
-
+  displayProducts(products) {
+    console.log(products);
+    let result = '';
+    products.forEach(product => {
+      result += `
+      <article class="product">
+        <div class="img-container">
+          <img src=${product.img} alt=${product.id} class="product-img">
+          <button class="bag-btn" data-id=${product.id}>
+            <i class="fas fa-shopping-cart"></i>
+            add to bag
+          </button>
+        </div>
+        <h3>${product.title}</h3>
+        <h4>$${product.price}</h4>
+      </article>
+      `
+    })
+    productsDOM.innerHTML = result;
+  }
 }
 
 // localStorage 
-class localStorage{
+class Storage{
 
 }
 
@@ -35,4 +69,8 @@ class localStorage{
 document.addEventListener('DOMContentLoaded', () => {
   const ui = new UI();
   const products = new Products();
+
+
+  // get all products 
+  products.getProducts().then(products => ui.displayProducts(products));
 })
